a99410d51ae69cc5522249ce1f3aaf740a2bb985
Geoff <geoffrey.nightingale@gmail.com>
2021-10-27 08:06:49 +0100

    updated pipeline functionality

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from config import *
-from pipeline import loan_pipe
+from pipeline import SklearnPipeline
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
-
+from sklearn.linear_model import LogisticRegression
+from lightgbm import LGBMClassifier
 
 def create_datasets(train, target):
     """Create datasets."""
@@ -16,15 +17,20 @@ def calculate_performance(y_true, y_score):
 
     return 2 * roc_auc_score(y_true, y_score) - 1
 
-def main():
-    """Main function."""
-
-    # Load datasets and split
-    train = pd.read_csv(TRAIN_DATA_LOCATION)
-    X, y = create_datasets(train, TARGET)
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+def run_pipeline_and_score(X_train,
+                           y_train,
+                           X_val,
+                           y_val,
+                           convert_categorical_to_ordinal,
+                           mean_imputation,
+                           algorithm,
+                           label=""):
+    """Run the pipeline and score performance metrics."""
 
     # Run pipeline
+    loan_pipe = SklearnPipeline(convert_categorical_to_ordinal=convert_categorical_to_ordinal,
+                                mean_imputation=mean_imputation,
+                                algorithm=algorithm).make_pipeline()
     loan_pipe.fit(X_train, y_train)
 
     # Score out train and test sets
@@ -35,8 +41,47 @@ def main():
     train_gini = calculate_performance(y_train, y_train_score)
     val_gini = calculate_performance(y_val, y_val_score)
 
+    if label != "":
+        print(label)
     print(f"train gini: {train_gini}")
-    print(f"val gini: {val_gini}")
+    print(f"val gini: {val_gini} \n")
+
+
+def main():
+    """Main function."""
+
+    # Load datasets and split
+    train = pd.read_csv(TRAIN_DATA_LOCATION)
+    X, y = create_datasets(train, TARGET)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+
+    # Run pipelines
+    run_pipeline_and_score(X_train,
+                           y_train,
+                           X_val,
+                           y_val,
+                           False,
+                           True,
+                           LogisticRegression(max_iter=10000),
+                           label="Logistic regression with mean imputation")
+    
+    run_pipeline_and_score(X_train,
+                           y_train,
+                           X_val,
+                           y_val,
+                           True,
+                           True,
+                           LogisticRegression(max_iter=10000),
+                           label="Logistic regression with mean imputation & ordinal encoding")
+
+    run_pipeline_and_score(X_train,
+                           y_train,
+                           X_val,
+                           y_val,
+                           True,
+                           True,
+                           LGBMClassifier(),
+                           label="LGBM with mean imputation & ordinal encoding")
 
 if __name__ == "__main__":
     main()
--- pipeline.py
+++ pipeline.py
@@ -3,27 +3,49 @@ from sklearn.linear_model import LogisticRegression
 from config import REPLACE_MISSING_VALUE, DROP_FEATURES, MAX_CARDINALITY
 import preprocessing as pp
 
-loan_pipe = Pipeline(
-    [
-        (
-            "drop_user_specified_features",
-            pp.DropFeatures(drop_features=DROP_FEATURES),
-        ),
-        (
-            "convert_categorical_vars_to_ordinal",
-            pp.ConvertCategoricalToOrdinal(max_cardinality=MAX_CARDINALITY),
-        ),
-        (
-            "drop_non_numeric_features",
-            pp.DropNonNumericFeatures(),
-        ),
-        (
-            "mean_imputation",
-            pp.MeanImputer(),
-        ),
-        (
-            "gb_model",
-            LogisticRegression(),
-        ),
-    ]
-)
+class SklearnPipeline:
+    """Class to construct an Sklearn pipeline."""
+    def __init__(self, 
+                 convert_categorical_to_ordinal=True,
+                 mean_imputation=True,
+                 algorithm=LogisticRegression()
+                ):
+
+        self.convert_categorical_to_ordinal = convert_categorical_to_ordinal
+        self.mean_imputation = mean_imputation
+        self.algorithm = algorithm
+
+    def make_pipeline(self):
+        """Make the pipeline."""
+
+        pipe_list = [(
+                    "drop_user_specified_features",
+                    pp.DropFeatures(drop_features=DROP_FEATURES),
+                )]
+
+        if self.convert_categorical_to_ordinal:
+            pipe_list.append((
+                             "convert_categorical_vars_to_ordinal",
+                             pp.ConvertCategoricalToOrdinal(max_cardinality=MAX_CARDINALITY),
+                            ))
+
+        # Always drop the non-numeric features!
+        pipe_list.append((
+                          "drop_non_numeric_features",
+                          pp.DropNonNumericFeatures(),
+                         ))
+
+        if self.mean_imputation:
+            pipe_list.append((
+                              "mean_imputation",
+                              pp.MeanImputer(),
+                            ))
+                            
+        # Add the model to the pipeline
+        pipe_list.append((
+                          "model",
+                          self.algorithm,
+                         ))
+
+        return Pipeline(pipe_list,)
+        
